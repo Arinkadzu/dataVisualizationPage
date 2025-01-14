3a331f27867fff9f3b396fd7d4e913f79389d058
Arina Fokina <arina.fokina11@gmail.com>
2025-01-14 22:45:22 +0200

    try to fix

--- src/js/pages/site.js
+++ src/js/pages/site.js
@@ -2,12 +2,12 @@
 import NavigationComponent from '../../dist/js/components/navigationComponent.js'//'../../dist/js/components/navigationComponent.js';
 import VisualizationComponnet from '../../dist/js/components/vizualizationComponent.js';
 import ThemeManager from '../../dist/js/components/darkModeComponent.js';
-import ProgressLineComponent from '../../dist/js/components/ProgressLineComponent.js';
+//import ProgressLineComponent from '../../dist/js/components/ProgressLineComponent.js';
 
 // Initialize when document is ready
 document.addEventListener('DOMContentLoaded', () => {
-    const progressLine = new ProgressLineComponent('.progress-line');
-    progressLine.init();
+    // const progressLine = new ProgressLineComponent('.progress-line');
+    // progressLine.init();
     const vizManager = new VisualizationComponnet();
     vizManager.init();
     const dropdown = new NavigationComponent();
